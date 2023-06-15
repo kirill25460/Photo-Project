8111c8f722ee0d746d957bed4885cb22da5e362e
Vlad Melnik <vmelniki2828@gmail.com>
2023-06-15 19:58:51 +0300

    jnjnj

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,37 +1,16 @@
+import { useState } from 'react';
 import styles from './AdminPanel.module.css'
-import AdminPanelBtn from 'components/AdminPanelBtn/AdminPanelBtn'
-import AdminPanelInput from 'components/AdminPanelInput/AdminPanelInput'
-import AdminWorkSpace from 'components/AdminWorkSpace/AdminWorkSpace'
-import { useState, useEffect } from 'react'
-
-
-const asd = {
-    login: 'Natasha',
-    password:'natashadmin'
-}
+import AdminAutorisation from 'components/AdminAutorisation/AdminAutorisation';
+import AdminWorkSpace from 'components/AdminWorkSpace/AdminWorkSpace';
 
 const AdminPanel = () => {  
+    const [isLoading, setLoading] = useState()
 
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [auth, setAuth] = useState({});
-    const [isLoading, setLoading] = useState(true)
 
-    const getAuth = () => {
-        setAuth({ login, password });
-        setLoading(false)
-    }
-
-    useEffect(() => {
-        console.log(auth);
-      }, [auth]);
 
     return (
         <>
-            {!isLoading ? null : <AdminPanelInput text={'Write your email'} type={'email'} value={login || ''} setValue={setLogin}/>}
-            {!isLoading ? null : <AdminPanelInput text={'Write your password'} type={'password'} value={password || ''}  setValue={setPassword}/>}
-            {!isLoading ? null : <AdminPanelBtn getAuth={getAuth} />}
-            { asd.login === auth.login && asd.password === auth.password ? <AdminWorkSpace/> : null }
+            {!isLoading ? <AdminAutorisation setLoading={setLoading}/> :  <AdminWorkSpace/>}
         </>
     )
 }
