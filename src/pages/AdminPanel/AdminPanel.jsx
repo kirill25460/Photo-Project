8f572cbca22dfb53129852a911734e4fde07aed5
Vlad Melnik <vmelniki2828@gmail.com>
2023-06-15 20:32:33 +0300

    Update AdminPanel.jsx

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -9,7 +9,6 @@ const AdminPanel = () => {
     return (
         <>
             {!isLoading ? <AdminAutorisation setLoading={setLoading}/> :  <AdminWorkSpace/>}
-            
         </>
     )
 }
