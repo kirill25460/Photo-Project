cf64b9283139f163be8ab5bc0bc1ff4e88c52dbb
Vlad Melnik <vmelniki2828@gmail.com>
2023-06-15 20:41:30 +0300

    asd

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import styles from './AdminPanel.module.css'
 import AdminAutorisation from 'components/AdminAutorisation/AdminAutorisation';
 import AdminWorkSpace from 'components/AdminWorkSpace/AdminWorkSpace';
 
