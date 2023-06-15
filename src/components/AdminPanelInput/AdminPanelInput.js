85ee632a35b44d449b0deb583d850292f37c307e
MazhitKaisar0909 <124054252+MazhitKaisar0909@users.noreply.github.com>
2023-06-15 20:27:05 +0300

    Update AdminPanelInput.js

--- src/components/AdminPanelInput/AdminPanelInput.js
+++ src/components/AdminPanelInput/AdminPanelInput.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import styles from './AdminPanelInput.module.css'
 import { useState } from 'react'
 
 const AdminPanelBtn = ({ text, type, className, getAuth}) => {
